3c0eb240e7f47ad5926016c8e2f20d7752a86345
dekim1028 <daeun235194@gmail.com>
2021-04-02 12:58:25 +0900

    fix:typo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>HELLO6</p>
+        <p>HELLO7</p>
         <a
           className="App-link"
           href="https://reactjs.org"
